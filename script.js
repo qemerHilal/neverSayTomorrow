464279c349852514853976ca3c1013f1afc4e94e
Nuria <qemer2030@gmail.com>
2025-09-30 16:07:41 +0300

    changes to landing page made

---
+++ script.js
@@ -0,0 +1,13 @@
+function hamMenu()
+{
+    const cc=document.getElementById("bars");
+   if(cc.className!="")
+    {
+      cc.classList.toggle("show");
+      
+    } 
+    else{
+        //do nothing
+    }
+
+}
